c73d7a62a07e347e38fc3596536fc70a905bb488
Mateusz Tomaszkiewicz <ressetpublic@gmail.com>
2018-02-14 01:00:49 +0100

    Add second ADC channel
    Now we have period (working) and duration (not just yet).

--- main.c
+++ main.c
@@ -14,11 +14,10 @@ int main(void)
     DDRB |= _BV(PB6) | _BV(PB7) | _BV(PB0);
 
     PORTD &= ~(_BV(PD5) | _BV(PD6) | _BV(PD7));
-    PORTB &= ~(_BV(PB6) | _BV(PB7));
-    PORTB |= _BV(PB0);
+    PORTB &= ~(_BV(PB6) | _BV(PB7) | _BV(PB0));
+    //PORTB |= _BV(PB0);
 
     ADCSRA |= _BV(ADEN);
-    ADMUX |= _BV(REFS0) | _BV(MUX2);
 
     TCCR1A = 0x03;
     TCCR1B = 0x1b;
@@ -29,20 +28,36 @@ int main(void)
     sei();
 
     while (1) {
+        // Main period, ADC0
+        ADMUX = _BV(REFS0);
         ADCSRA |= _BV(ADSC);
         loop_until_bit_is_clear(ADCSRA, ADSC);
-        uint16_t a = 0;
-        a = ADCL;
-        a |= ADCH << 8;
-        a += 4;
+        uint16_t period = 0;
+        period = ADCL;
+        period |= ADCH << 8;
+        // We convert 0 - 1023 range to 600 - 3669
+        period = period * 3 + 600;
 
-        if (counter > a) {
+        // Duration of ON stat, ADC1
+        ADMUX = _BV(REFS0) | _BV(ADLAR) | _BV(MUX1);
+        ADCSRA |= _BV(ADSC);
+        loop_until_bit_is_clear(ADCSRA, ADSC);
+        uint8_t duration = 0;
+        duration = ADCH;
+        // We convert 0 - 255 range to 2 - 10
+        duration = (duration >> 5) + 2;
+
+        if (counter > period) {
             cli();
             counter = 0;
             sei();
 
             PORTD |= _BV(PD7);
-            _delay_ms(500);
+            uint8_t i = 0;
+            while (i < duration) {
+                _delay_ms(100);
+                i++;
+            }
             PORTD &= ~_BV(PD7);
         }
     }
@@ -67,7 +82,9 @@ int main(void)
 
 ISR(TIMER1_OVF_vect)
 {
-    PORTB = PORTB ^ _BV(PB0);
+    PORTB |= _BV(PB0);
+    _delay_ms(50);
+    PORTB &= ~_BV(PB0);
 
     counter++;
 }
