4f7646a4ef079065038c9c9778ae0af414657944
Mateusz Tomaszkiewicz <ressetpublic@gmail.com>
2018-02-21 23:12:41 +0100

    Add UART functions, add initial ON state

--- main.c
+++ main.c
@@ -8,6 +8,24 @@
 
 volatile uint16_t counter = 0;
 
+void uart_init()
+{
+    #define BAUD 9600                                   // define baud
+    #define BAUDRATE ((F_CPU)/(BAUD*16UL)-1)            // set baud rate value for UBRR
+    UBRRH = (BAUDRATE>>8);                      // shift the register right by 8 bits
+    UBRRL = BAUDRATE;                           // set baud rate
+    UCSRB|= (1<<TXEN)|(1<<RXEN);                // enable receiver and transmitter
+    UCSRC|= (1<<URSEL)|(1<<UCSZ0)|(1<<UCSZ1);   // 8bit data format
+}
+
+void uart_send_byte_nl(uint8_t byte)
+{
+        while (!(UCSRA & (1 << UDRE)));
+        UDR = byte;
+        while (!(UCSRA & (1 << UDRE)));
+        UDR = '\n';
+}
+
 int main(void)
 {
     DDRD |= _BV(PD5) | _BV(PD6) | _BV(PD7);
@@ -24,7 +42,11 @@ int main(void)
 
     TIMSK = _BV(TOIE1);
 
+    uart_init();
+
     sei();
+
+    uint8_t first_run = 1;
 
     while (1) {
         // Main period, ADC0
@@ -36,6 +58,8 @@ int main(void)
         period |= ADCH << 8;
         // We convert 0 - 1023 range to 600 - 3669
         period = period * 3 + 600;
+        //uart_send_byte_nl(period & 0xff);
+        //period = 20;
 
         // Duration of ON stat, ADC1
         ADMUX = _BV(REFS0) | _BV(MUX1);
@@ -44,18 +68,25 @@ int main(void)
         uint16_t duration = 0;
         duration = ADCL;
         duration |= ADCH << 8;
-        // We convert 0 - 1023  range to 2 - 10
-        duration = (duration >> 7) + 2;
+        // We convert 0 - 1023  range to 2 - 10, add +10 to eventualy reach 10 as a max
+        duration = ((duration + 10) >> 7) + 2;
 
-        if (counter > period) {
+        //uart_send_byte_nl(duration & 0xff);
+
+        if (first_run == 1 || counter > period) {
+            first_run = 0;
+
             cli();
             counter = 0;
             sei();
 
             PORTD |= _BV(PD7);
+
+            // uart_send_byte_nl(duration & 0xff);
+
             uint8_t i = 0;
             while (i < duration) {
-                _delay_ms(1000);
+                _delay_ms(950);
                 i++;
             }
             PORTD &= ~_BV(PD7);
